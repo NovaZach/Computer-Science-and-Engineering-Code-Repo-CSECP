6480f47649c2d840b129b084c916dc4d75d1f8b5
NovaZach <134723867+NovaZach@users.noreply.github.com>
2024-09-24 10:08:47 +0530

    Grade_Maker.java

---
+++ OOPS_Java/Grade_Maker.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class GradeMaker {
+public static void main (String[] args)
+{
+int mark;
+char grade;
+Scanner sc = new Scanner (System.in);	
+System.out.print("enter the mark ");
+mark= sc.nextInt();
+if( mark>90 )
+{grade = 'A';}
+else if( mark>70 )
+{grade = 'B';}	
+else 
+{grade = 'F';}	
+System.out.print("grade is "+grade);
+System.out.print("\n");
+sc.close();}}
